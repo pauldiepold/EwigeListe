acecd20fd0e434caeef972045ec7ad5f3e2a65cf
Paul Diepold <paul.diepold@yahoo.de>
2019-04-12 18:27:35 +0200

    add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('img/favicon_32x32.png') }}" sizes="32x32" />
+    <link rel="icon" href="{{ asset('img/favicon_192x192.png') }}" sizes="192x192" />
 
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
