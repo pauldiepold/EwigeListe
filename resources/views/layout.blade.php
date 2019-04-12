b8349c598deb7e86f5fab40ad125131e0b35f086
Paul Diepold <paul.diepold@yahoo.de>
2019-04-12 17:10:41 +0200

    2019-04-12 #2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,74 +3,79 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}"></script>
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link href="{{ asset('css/style.css') }}?v={{ time() }}" rel="stylesheet" type="text/css" >
 
     <title>@yield('title')</title>
 </head>
 <body class="text-center">
-	<script>
-		$(function() {
- 			$('#navbarCollapse').on('show.bs.collapse', function () {
- 				$('#fa-bars').hide();
-				$('#fa-times').show();
-			});
-			$('#navbarCollapse').on('hide.bs.collapse', function () {
- 				$('#fa-bars').show();
-				$('#fa-times').hide();
-			});
-		});
-	</script>
-	<div id="navbar-header" class="w-100">
-		<div class="d-flex justify-content-center">
-			<h3 class="text-danger text-center">Ewige Liste</h3>
-		</div>
-	</div>
+    <script>
+        $(function () {
+            $('#navbarCollapse').on('show.bs.collapse', function () {
+                $('#fa-bars').hide();
+                $('#fa-times').show();
+            });
+            $('#navbarCollapse').on('hide.bs.collapse', function () {
+                $('#fa-bars').show();
+                $('#fa-times').hide();
+            });
+        });
+    </script>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">			
-        	<i id="fa-bars" class="fas fa-bars"></i><i id="fa-times" class="fas fa-times" style="display:none;"></i>
-		<!--<span style="vertical-align: middle;"> Menü</span> -->
-        </button>
+        <div class="row w-100 justify-content-center mx-auto">
+            <div class="col text-center text-light">
+                <h3>Ewige Liste</h3>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+                <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse"
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                    <i id="fa-bars" class="fas fa-bars"></i>
+                    <i id="fa-times" class="fas fa-times" style="display:none;"></i>
+                </button>
 
-        <div class="collapse navbar-collapse justify-content-md-center" id="navbarCollapse">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/rounds/create">Neue Runde starten</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/players">Ewige Liste</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/rounds">Rundenarchiv</a>
-                </li>
-				
-				<li class="nav-item">
-					@guest
-                    <a class="nav-link" href="/login">Login</a>
-					@endguest
-					@auth
-					<a class="nav-link" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                <div class="collapse navbar-collapse justify-content-md-center" id="navbarCollapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/players">Ewige Liste</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/rounds/create">Neue Runde starten</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/rounds">Rundenarchiv</a>
+                        </li>
+
+                        <li class="nav-item">
+                            @guest
+                                <a class="nav-link" href="/login">Login</a>
+                            @endguest
+                            @auth
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-					@endauth
-                </li>
-				
-            </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            @endauth
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         </div>
+
+
     </nav>
     <div class="container">
         <h3 class="mb-4">
@@ -85,11 +90,5 @@
         </div>
     </footer>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
 </body>
 </html>
