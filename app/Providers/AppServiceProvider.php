94b9d54ccb265d981ddc4963f9dc4953791b2141
Paul Diepold <paul.diepold@yahoo.de>
2019-04-22 23:52:09 +0200

    2019-04-22 #6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Paginator::defaultView('pagination.pagination');
         date_default_timezone_set('Europe/Berlin');
+        setlocale(LC_TIME, 'German');
     }
 }
