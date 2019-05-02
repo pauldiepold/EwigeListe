75f4c62f692a06d70c9e5b4b1958b465f7beb774
Paul Diepold <paul.diepold@yahoo.de>
2019-05-02 20:36:35 +0200

    2019-05-02 #1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
@@ -30,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Paginator::defaultView('pagination.pagination');
         date_default_timezone_set('Europe/Berlin');
+        Carbon::setLocale(config('app.locale'));
         setlocale(LC_TIME, 'German');
     }
 }
