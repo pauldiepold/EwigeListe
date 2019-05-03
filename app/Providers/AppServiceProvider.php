c78ed9c671759d24f04bda08bf65490140b2a427
Paul Diepold <paul.diepold@yahoo.de>
2019-05-03 23:56:37 +0200

    2019-05-03 #4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(TelescopeServiceProvider::class);
-        }
     }
 
     /**
