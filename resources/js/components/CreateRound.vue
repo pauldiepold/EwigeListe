fd7b2b493a06b4c9fda02890409e89a4a9c8b069
Paul Diepold <paul.diepold@yahoo.de>
2019-06-05 18:43:05 +0200

    working on vue feature

--- resources/js/components/CreateRound.vue
+++ resources/js/components/CreateRound.vue
@@ -1,24 +1,39 @@
 <template>
     <div class="form-autocomplete">
-        <div class="form-group">
-            <input class="form-control mx-auto" style="max-width: 15rem;" v-model="textSearch" type="text"
-                   :placeholder="placeholder" @focus="showOptions"/>
-        </div>
+        <div class="bg-white rounded shadow-2 mx-auto my-4 p-3" style="max-width:19rem;" v-if="players.length < 7">
+            <input class="custom-input" :value="textSearch" @input="textSearch = $event.target.value" type="text"
+                   :placeholder="placeholder" @focus="show = true" @blur="show = true"/>
 
-        <div class="players-list" v-show="show">
-            <div class="player" v-for="(player, index) in filteredOptions" @click="addPlayer(player)">
-                {{player.name}}
+            <div class="players-list" v-show="show">
+                <div class="player font-weight-bold mt-3 mx-1 text-left" v-for="(player, index) in filteredOptions"
+                     @click="addPlayer(player)">
+                    {{player.name}}
+                </div>
+                <p v-if="filteredOptions.length===0">Spieler wurde nicht gefunden</p>
             </div>
-            <p v-if="filteredOptions.length===0">Spieler wurde nicht gefunden</p>
         </div>
 
-        <hr>
-        <p class="font-weight-bold">Ausgewählte Spieler:</p>
+
+        <h4 class="mt-4" v-if="players.length !== 0">Ausgewählte Spieler:</h4>
         <div class="players-selected">
-            <div class="player-selected" v-for="(player, key) in players">
-                {{player.name}} <button class="btn btn-link" @click="removePlayer(key)">Entfernen</button>
-            </div>
+            <transition-group name="flip-list">
+                <div class="player-selected rounded bg-white px-3 py-2 my-3 mx-auto d-flex align-items-center justify-content-between shadow-2"
+                     style="max-width: 19rem;"
+                     v-for="(player, key) in players" v-bind:key="player.id">
+                <span class="font-weight-bold">
+                {{player.name}}
+                </span>
+                    <span style="font-size: 1.1rem;">
+                    <i class="fas fa-chevron-up mx-1 text-muted" @click="moveUp(player)" v-if="key !== 0"></i>
+                    <i class="fas fa-chevron-down mx-1 text-muted" @click="moveDown(player)"
+                       v-if="key !== players.length-1"></i>
+                    <i class="fas fa-trash mx-1 text-danger" @click="removePlayer(key)"></i>
+                </span>
+                </div>
+            </transition-group>
         </div>
+
+        <button class="btn btn-primary mt-4" :disabled="players.length < 4">Neue Runde starten</button>
     </div>
 </template>
 
@@ -45,7 +60,7 @@
         },
         computed: {
             filteredOptions() {
-                return this.allPlayers.filter(player => player.name.toLowerCase().includes(this.textSearch.toLowerCase()) && !this.inSelectedPlayers(player.id)).slice(0, 7);
+                return this.allPlayers.filter(player => player.name.toLowerCase().includes(this.textSearch.toLowerCase()) && !this.inSelectedPlayers(player.id)).slice(0, 5);
             },
             selectedPlayers() {
                 return this.players.map(v => v.id);
@@ -55,6 +70,16 @@
         methods: {
             inSelectedPlayers(id) {
                 return this.selectedPlayers.includes(id);
+            },
+            moveUp(player) {
+                let tempIndex = this.players.indexOf(player);
+                this.players.splice(tempIndex, 1);
+                this.players.splice(tempIndex - 1, 0, player);
+            },
+            moveDown(player) {
+                let tempIndex = this.players.indexOf(player);
+                this.players.splice(tempIndex, 1);
+                this.players.splice(tempIndex + 1, 0, player);
             },
             showOptions() {
                 this.show = true;
