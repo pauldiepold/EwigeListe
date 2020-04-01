d6b24c48f93dabb1b7ca36027720c3450a9b61cf
Paul Diepold <paul.diepold@yahoo.de>
2020-04-01 11:54:10 +0200

    ShouldBroadcastNow

--- app/Events/LiveGameDataBroadcasted.php
+++ app/Events/LiveGameDataBroadcasted.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\LiveGame;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class LiveGameDataBroadcasted implements ShouldBroadcast
+class LiveGameDataBroadcasted implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
