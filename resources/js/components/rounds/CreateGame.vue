5285060fb96baef7a22a44933821cc638d91bbd4
Paul Diepold <paul.diepold@yahoo.de>
2020-11-16 03:11:22 +0100

    disable double tap zoom on create game

--- resources/js/components/rounds/CreateGame.vue
+++ resources/js/components/rounds/CreateGame.vue
@@ -8,7 +8,7 @@
         <div class="modal" id="createGame" tabindex="-1" role="dialog" aria-labelledby="createModalLabel"
              aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
+                <div class="modal-content disable-dbl-tap-zoom">
                     <div class="modal-header">
                         <h5 class="modal-title mx-auto" id="createModalLabel">Welche Spieler haben gewonnen?</h5>
                     </div>
