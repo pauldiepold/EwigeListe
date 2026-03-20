43ad0b77290df2e6c6bdc8af0fb417979624dc53
Paul Diepold <paul.diepold@outlook.de>
2026-03-20 23:36:48 +0100

    update echo and pusher

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const herdDevHost = 'ewigeliste.test';
+
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            detectTls: herdDevHost,
         }),
         vue({
             template: {
